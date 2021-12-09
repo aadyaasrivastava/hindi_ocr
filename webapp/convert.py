d9803fb388d9b99559a0ac0d0f4ba241920189b3
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-12-09 11:57:10 +0530

    Update convert.py

--- webapp/convert.py
+++ webapp/convert.py
@@ -1,38 +1,29 @@
-import easyocr
-from numpy import asarray
-from PIL import Image
 import docx
-import uuid
 from docx.shared import Pt
 from fpdf import FPDF, fpdf
-DOWNLOAD_FOLDER = '../webapp/download/document/'
-def converter(img):
-    image = Image.open(img)
-    data = asarray(image)
-    reader = easyocr.Reader(['hi'])
-    result = reader.readtext(data,detail = 1)
-    ans = ""
-    for i in result:
-        ans+=i[1]
-        ans+=' '
+
+def converter(txt):
+    DOWNLOAD_FOLDER = 'static/Download/document/'
     doc = docx.Document()
-    para = doc.add_paragraph().add_run(ans)
+    para = doc.add_paragraph().add_run(txt)
     para.font.size = Pt(12)
-    filename = str(uuid.uuid4())
-    doc.save(DOWNLOAD_FOLDER+ "filename"+".docx")
-DOWNLOAD_FOLDER = '../webapp/download/pdf/'
-def converter_1(img):
-    image = Image.open(img)
-    data = asarray(image)
-    reader = easyocr.Reader(['hi'])
-    result = reader.readtext(data,detail = 1)
-    ans = ""
-    for i in result:
-        ans+=i[1]
-        ans+=' '
-    document=FPDF()
-    document.add_page()
-    document.cell(400, 50, ln=1, align="L")
-    document.output("pdf_file_name.pdf")
-    document=FPDF(orientation='P', unit='mm', format='A3''A2''A1')
-    fpdf.save(DOWNLOAD_FOLDER+ "filename"+".pdf")
+    # filename = str(uuid.uuid4())
+    doc.save(DOWNLOAD_FOLDER+"hindi_doc.docx")
+
+def converter_1(txt):
+    DOWNLOAD_FOLDER = 'static/Download/pdf/'
+    
+    # document=FPDF()
+    # document.add_page()
+    # document.cell(400, 50, txt, ln=1, align="L")
+    # document.output("pdf_file_name.pdf")
+    # document=FPDF(orientation='P', unit='mm', format='A3''A2''A1')
+    # fpdf.save(DOWNLOAD_FOLDER+ "filename"+".pdf")
+
+    pdf = FPDF()
+    pdf.add_font('gargi', '', 'gargi.ttf', uni=True)
+    pdf.set_font('gargi', '', 14)
+    pdf.add_page()
+    # pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt=txt, ln=1, align="C")
+    pdf.output(DOWNLOAD_FOLDER + "hindi_pdf.pdf")
