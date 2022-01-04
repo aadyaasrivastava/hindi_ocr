77ce1d624165525e0b6065d6f1ddaee00ea3fde2
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2022-01-04 21:17:40 +0530

    Update easy_ocr.py

--- webapp/easy_ocr.py
+++ webapp/easy_ocr.py
@@ -2,14 +2,20 @@ import easyocr
 from numpy import asarray
 import numpy
 import cv2
+from PIL import Image
+from PIL import ImageFilter
 
 def get_text(image):
     
     imagecv = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
+    
+    
 
-    data = asarray(image)
+    pil_image = image.filter(ImageFilter.EDGE_ENHANCE)
+    #pil_image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    data = asarray(pil_image)
     reader = easyocr.Reader(['hi'])
-    result = reader.readtext(data)
+    result = reader.readtext(data, batch_size=8)
 
     text = []
     coo = []
@@ -21,15 +27,11 @@ def get_text(image):
         x_min, y_min = [int(min(idx)) for idx in zip(*cord)]
         x_max, y_max = [int(max(idx)) for idx in zip(*cord)]
 
-        # result = ((coo,text,per),(coo,text,per),(coo,text,per),.........)
-
-        # i = (coo,text,per)
-
-        # text = (text,per)
+    
 
         text.append(i[1:])
 
         coo.append([(x_min, y_min), (x_max, y_max)])
 
-
-    return(text,coo)
+		
+    return(text,coo,pil_image)
